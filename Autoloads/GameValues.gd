28214fe5a31b72601c5fc8678444ac5459a29ec4
storm <stormpower44@protonmail.com>
2023-08-27 20:51:19 -0600

    add level select

--- Autoloads/GameValues.gd
+++ Autoloads/GameValues.gd
@@ -6,3 +6,13 @@ func move_to_level(level):
 	match level:
 		0:
 			get_tree().change_scene_to_file("res://levels/level0.tscn")
+		1:
+			get_tree().change_scene_to_file("res://levels/level1.tscn")
+		2:
+			get_tree().change_scene_to_file("res://levels/level2.tscn")
+		3:
+			get_tree().change_scene_to_file("res://levels/level3.tscn")
+		4:
+			get_tree().change_scene_to_file("res://levels/level4.tscn")
+		5:
+			get_tree().change_scene_to_file("res://levels/level5.tscn")
